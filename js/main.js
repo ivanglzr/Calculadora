059d0fb594d9a5be0bc7e7f57107104d8e6e3c42
Ivan <ivanglzr01@gmail.com>
2023-08-12 18:00:56 +0200

    Calculadora sencilla realizada

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let btns = document.querySelectorAll(".btn")
+let operation = document.querySelector("#operation")
+let btnEqual = document.querySelector(".btn-equal")
+let btnDelete = document.querySelector(".btn-delete")
+
+for(let btn of btns){
+  btn.addEventListener("click", e => {
+    operation.value += e.target.innerHTML
+  })
+}
+
+btnEqual.addEventListener("click", ()=>{
+  let result
+
+  try {
+
+    result = eval(operation.value)
+
+    if(result == NaN){
+      return alert("La operacion no se puede realizar")
+    }
+
+    operation.value = result
+    
+  } catch (error) {
+    alert("La operacion no es valida")
+  }
+})
+
+btnDelete.addEventListener("click", ()=>{
+  operation.value = operation.value.substring(0, operation.value.length - 1)
+})
